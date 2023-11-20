1ef3d7fd4fb906a17ec6eecb4511d1ce5ecf1dac
joshua-dixon1 <143402358+joshua-dixon1@users.noreply.github.com>
2023-11-20 13:25:20 +0000

    Add files via upload

---
+++ Charging Data Plots V2 (refined).py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def print_versions():
+    """
+    Function to print versions of libraries being used. Function used for 
+    formatting only
+    """
+    libraries = {'Pandas': pd, 'NumPy': np, 'Matplotlib': plt.matplotlib, 'Seaborn': sns}
+    for lib, module in libraries.items():
+        print(f"{lib} version: {module.__version__}")
+        
+    libraries= 1
+
+def plot_time_series(dataframe, time_col, data_cols, title_prefix):
+    """
+    Function to plot time series graphs for multiple data columns.
+    """
+    for data_col in data_cols:
+        plt.figure(figsize=(10, 5))
+        plt.plot(dataframe[time_col], dataframe[data_col], label=f'{data_col} over Time')
+        plt.xlabel('Time [s]')
+        plt.ylabel(data_col)
+        plt.title(f'{title_prefix} {data_col}')
+        plt.legend()
+        plt.show()
+
+def descriptive_statistics(dataframe, columns):
+    """
+    Function to print descriptive statistics of multiple dataframe columns.
+    """
+    for column in columns:
+        print(f"Descriptive statistics for {column}:")
+        print(dataframe[column].describe(), "\n")
+
+# Print the versions of the libraries being used
+print_versions()
+
+# Load Raw Charging Data
+charging_data_path = 'C:\\Users\\joshu\\Desktop\\Charging Data\\Raw Data\\2515_230314_5CsT_ch008_007NCR3B_no header.txt'
+
+# Attempt to read the TXT file as CSV
+try:
+    df = pd.read_csv(charging_data_path, sep=',')
+    print(df.head())
+
+    # Plotting time series data for multiple columns
+    columns_to_plot = ['U[V]', 'I[A]', 'Ah[Ah]', 'Wh[Wh]']
+    plot_time_series(df, '~Time[s]', columns_to_plot, 'Plot for')
+
+    # Printing descriptive statistics for each measurement
+    descriptive_statistics(df, columns_to_plot)
+    
+except FileNotFoundError:
+    print(f"File not found: {charging_data_path}")
+except pd.errors.ParserError:
+    print(f"Parser error in file: {charging_data_path}")
+except Exception as e:
+    print(f"An error occurred: {e}")
---
+++ Charging Data Plots.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def print_versions():
+    """
+    Function to print versions of libraries being used. Function used for 
+    formatting only
+    """
+    print("\n")  # For better readability in output
+    print(f"Pandas version: {pd.__version__}")
+    print(f"NumPy version: {np.__version__}")
+    print(f"Matplotlib version: {plt.matplotlib.__version__}")
+    print(f"Seaborn version: {sns.__version__}")
+    print("\n")  # For better readability in output
+
+def plot_time_series(dataframe, time_col, data_col, ylabel, title):
+    """
+    Function to plot a time series graph.
+    """
+    plt.figure(figsize=(10,5))
+    plt.plot(dataframe[time_col], dataframe[data_col], label=f'{ylabel} over Time')
+    plt.xlabel('Time [s]')
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.legend()
+    plt.show()
+
+def descriptive_statistics(dataframe, column):
+    """
+    Function to print descriptive statistics of a dataframe column.
+    """
+    print(f"Descriptive statistics for {column}:")
+    print(dataframe[column].describe())
+    print("\n")  # For better readability in output
+    
+# Print the versions of the libraries being used
+print_versions()
+
+# Load Raw Charging Data
+charging_data_path = r'C:\Users\joshu\Desktop\Charging Data\Raw Data\2515_230314_5CsT_ch008_007NCR3B_no header.txt'
+
+# Attempt to read the TXT file as CSV
+try:
+    # Adjust the separator if needed 
+    df = pd.read_csv(charging_data_path, sep=',')  
+    
+    # Check the first few rows to confirm it's loaded correctly
+    print(df.head())
+
+    # Plotting time series data
+    plot_time_series(df, '~Time[s]', 'U[V]', 'Voltage [V]', 'Voltage Plot')
+    plot_time_series(df, '~Time[s]', 'I[A]', 'Current [A]', 'Current Plot')
+    plot_time_series(df, '~Time[s]', 'Ah[Ah]', 'Ampere-hour [Ah]', 'Ampere-hour Plot')
+    plot_time_series(df, '~Time[s]', 'Wh[Wh]', 'Watt-hour [Wh]', 'Watt-hour Plot')
+    
+    # Printing descriptive statistics for each measurement
+    descriptive_statistics(df, 'U[V]')
+    descriptive_statistics(df, 'I[A]')
+    descriptive_statistics(df, 'Ah[Ah]')
+    descriptive_statistics(df, 'Wh[Wh]')
+    
+except FileNotFoundError:
+    print(f"The file {charging_data_path} does not exist.")
+except pd.errors.ParserError:
+    print(f"Error parsing the file {charging_data_path}. Check the separator.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+    
+    
